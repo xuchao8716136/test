dc03d321a70314fabdffea58a5bd65b3dd638a0c
xuchao <xuchao8716136@gmail.com>
2016-11-15 15:01:20 +0800

    构造函数

--- javascripts/jscore.js
+++ javascripts/jscore.js
@@ -1,20 +1,48 @@
-/**
- * Created by xuchao on 2016/11/15.
- */
+// prototype
+// var a = {
+//     x:10,
+//     calculate:function(z){
+//         return this.x + this.y + z;
+//  }
+// };
+// var b = {
+//     y:20,
+//     _proto_:a
+// };
+//
+// var c = {
+//     y:20,
+//     _proto_:a
+// };
+//
+// b.calculate(30);
+// c.calculate(40);
 
+// constructor function
 
-var b = {
-    y:20,
-    _proto_:a
+function Foo(y) {
+    this.y = y;
+}
+
+Foo.prototype.x = 10;
+
+Foo.prototype.calculate = function (z) {
+    return this.x = this.y + z;
 };
 
-var c = {
-    y:20,
-    _proto_:a
-};
+var b = new Foo(30);
+var c = new Foo(30);
 
 b.calculate(30);
 c.calculate(40);
 
+console.log(
+    b._proto_ === Foo.prototype,
+    c._proto_ === Foo.prototype,
+    b.constructor === Foo,
+    c.constructor === Foo,
+    Foo.prototype.constructor === Foo,
 
-
+    b.calculate === b._proto_.calculate,
+    b._proto_.calculate === Foo.prototype.calculate
+);
