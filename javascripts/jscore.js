c4be1f301498bee2d6d54b7acd394a91bca53e10
xuchao <xuchao8716136@gmail.com>
2016-11-15 15:33:21 +0800

    构造函数

--- javascripts/jscore.js
+++ javascripts/jscore.js
@@ -20,29 +20,63 @@
 
 // constructor function
 
-function Foo(y) {
-    this.y = y;
+// function Foo(y) {
+//     this.y = y;
+// }
+//
+// Foo.prototype.x = 10;
+//
+// Foo.prototype.calculate = function (z) {
+//     return this.x = this.y + z;
+// };
+//
+// var b = new Foo(30);
+// var c = new Foo(30);
+//
+// b.calculate(30);
+// c.calculate(40);
+//
+// console.log(
+//     b._proto_ === Foo.prototype,
+//     c._proto_ === Foo.prototype,
+//     b.constructor === Foo,
+//     c.constructor === Foo,
+//     Foo.prototype.constructor === Foo,
+//
+//     b.calculate === b._proto_.calculate,
+//     b._proto_.calculate === Foo.prototype.calculate
+// );
+
+
+// es6
+
+class Foo{
+
+    constructor(name){
+        this._name = name;
+    }
+
+    getName(){
+        return this._name;
+    }
+}
+class Bar extends Foo{
+    getName(){
+        return super.getName() + 'Doe ';
+    }
 }
 
-Foo.prototype.x = 10;
+var bar = new Bar('John');
+console.log(bar.getName());
 
-Foo.prototype.calculate = function (z) {
-    return this.x = this.y + z;
-};
+// 作用域链
 
-var b = new Foo(30);
-var c = new Foo(30);
+var x = 10;
+(function () {
+    var y = 20;
+    (function bar() {
+        var z = 30;
+        console.log(x + y + z);
+    })()
+})();
 
-b.calculate(30);
-c.calculate(40);
-
-console.log(
-    b._proto_ === Foo.prototype,
-    c._proto_ === Foo.prototype,
-    b.constructor === Foo,
-    c.constructor === Foo,
-    Foo.prototype.constructor === Foo,
-
-    b.calculate === b._proto_.calculate,
-    b._proto_.calculate === Foo.prototype.calculate
-);
